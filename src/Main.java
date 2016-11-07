a0cec76785ed37ab278699b7d8d2ac1245fc8b81
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2016-11-07 18:10:25 +0000

    File input output not working properly. Troubleshooting required.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.io.*;
 /**
  * Created by Sara on 07-Nov-16.
  */
 
-//import java.io.*;
-
 public class Main {
 
     public static void main(String[] args) {
+
+        //Temp for variables
+        String path;
+
+
         System.out.println("Hello World");
 
         //An instance of the class needs to be created before the non-static methods can be referenced
@@ -15,6 +19,14 @@ public class Main {
 
         //calling the parSer method of the TestClass within the obj instance of the class
         obj.parSer();
+
+        //Stuff for fiddling- move in a bit
+        path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\160916_M00766_0079_000000000-ATNNU_16M13176_filtered_meta_annotated.vcf";
+        File vcf_file = new File(path);
+
+        //Instantiate second class
+        OpenVEPVCF obj2 = new OpenVEPVCF(vcf_file);
+        obj2.openFiles();
 
 
     }
--- src/OpenVEPVCF.java
+++ src/OpenVEPVCF.java
@@ -1,35 +1,38 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileReader;
 /**
  * Created by Sara on 07-Nov-16.
  */
 
-import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.io.FileReader;
 
 public class OpenVEPVCF {
 
+    //Stuff that is wanted
     private static final Logger Log = Logger.getLogger(OpenVEPVCF.class.getName());
 
     private File vcfFilePath;
+    private List<String> theRecords = new ArrayList<String>();
 
     public OpenVEPVCF(File vcfFilePath) {
         this.vcfFilePath = vcfFilePath;
     }
 
-    public void openFile()    {
 
-        Log.log(Level.INFO, "Parsing GTF file");
+    public void openFiles() {
+
+        Log.log(Level.INFO, "Opening VEP VCF file");
 
         String line;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(vcfFilePath))) {
-            while ((line = reader.readLine()) != null) {
-                System.out.println("line");
-            }
+        //try (BufferedReader reader = new BufferedReader(new FileReader(vcfFilePath))){
 
-        }
+        //}
     }
+}
 
 
 
