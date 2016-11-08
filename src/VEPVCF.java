1195d76b1ae1dba88bb376c2874f07524dbe5fef
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2016-11-08 19:12:27 +0000

    Extracted fields which require further parsing

--- src/VEPVCF.java
+++ src/VEPVCF.java
@@ -9,6 +9,8 @@ import htsjdk.variant.variantcontext.*;
 import htsjdk.variant.variantcontext.writer.*;
 import htsjdk.variant.vcf.*;
 
+import htsjdk.variant.utils.*;
+
 import java.util.List;
 
 /**
@@ -39,15 +41,51 @@ public class VepVcf {
         try(final VariantContextWriter writer = outputFile == null ? null : new VariantContextWriterBuilder().setOutputFile(outputFile).setOutputFileType(VariantContextWriterBuilder.OutputType.VCF).unsetOption(Options.INDEX_ON_THE_FLY).build();
             final AbstractFeatureReader<VariantContext, LineIterator> reader = AbstractFeatureReader.getFeatureReader(inputFile.getAbsolutePath(), new VCFCodec(), false)) {
             //while ((line = reader.readLine()) != null) {
-            System.out.println("line");
+            System.out.println("line"); //This is just in here for the moment to allow the try except block to work. Replace with better solution.
 
             //final ProgressLogger pl = new ProgressLogger(log, 1000000);
             for (final VariantContext vc : reader.iterator()) {
                 if (writer != null) {
                     writer.add(vc);
                 }
-                System.out.println(vc.getContig());
-                System.out.println(vc.getStart());
+                System.out.print(vc.getContig());
+                System.out.print("\t");
+                System.out.print(vc.getStart());
+                System.out.print("\t");
+                System.out.print(vc.getEnd()); //Could be useful for indels etc.
+                System.out.print("\t");
+                System.out.print(vc.getAttributeAsList("ID")); //This is returning null at present- no key found?
+                System.out.print("\t");
+                System.out.print(vc.getReference()); //Reference allele
+                System.out.print("\t");
+                System.out.print(vc.getAlleles()); //Returns all the potential alternate alleles- test with an indel
+                System.out.print("\t");
+                System.out.print(vc.getID());
+                System.out.print("\t");
+                System.out.print(vc.getPhredScaledQual());
+                System.out.print("\t");
+                System.out.print(vc.isFiltered());
+                System.out.print("\t");
+                System.out.print(vc.getAttribute("DP")); //Depth
+                System.out.print("\t");
+                //System.out.print(vc.getAttribute("CSQ")); //Long- commented out for now
+                System.out.print("\t");
+                //Need to find a better way of getting a transcript
+                //System.out.print(VariantContextUtils.match(VariantContext vc, ));
+                System.out.print("\t");
+
+                //System.out.print(vc.hasAttribute("Transcript")); //To see if a particular attribute is available as a key
+                System.out.print("\t");
+
+
+                //Requires further parsing
+                System.out.print(vc.getGenotypes());
+                System.out.print("\t");
+
+
+                //System.out.print(vc.getAttributes()); //Allows to obtain what is in the INFO field
+                System.out.print(vc.getAttributeAsString("CSQ","null"));
+                System.out.print("\n");
             }
 
         }catch(Exception e) {
