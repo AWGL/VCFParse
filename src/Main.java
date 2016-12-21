d78468eb6d7969c451a9cbd5b34e4bae0d58ed6d
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2016-12-21 16:44:41 +0000

    Testing GATK bug regarding allele ordering/numbering when joint calling

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,8 @@ public class Main {
         //path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\multisample_gatk3-6_altalleles.vcf";
         //path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\multisample_gatk3-6_altalleles_test.vcf";
         //path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\vcf.vcf";
-        path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\gatk_37.vcf";
+        path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\gatk_36.vcf";
+        path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\testingold.vcf";
         path="C:\\Users\\Sara\\Documents\\Work\\VCFtoTab\\ExampleFiles\\testing.vcf";
 
         //Creates a new File instance by converting given pathname string into an abstract pathname
