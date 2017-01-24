ec3f9ad8d55cb81c4cb02d13e95960ff370afc09
mcgml <mcgml@cardiff.ac.uk>
2017-01-24 14:27:30 +0000

    added apache cli

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 package nhs.genetics.cardiff;
 
+import org.apache.commons.cli.*;
+
 import java.io.*;
 import java.util.LinkedHashMap;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -19,8 +22,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine commandLine = null;
+        HelpFormatter formatter = new HelpFormatter();
+        Options options = new Options();
+
+        options.addOption("V", "Variant", true, "Path to input VCF file");
+
+        try {
+            commandLine = commandLineParser.parse(options, args);
+
+            if (!commandLine.hasOption("V")) throw new NullPointerException("Incorrect arguments");
+
+        } catch (ParseException | NullPointerException e){
+            formatter.printHelp(program + " " + version, options);
+            log.log(Level.SEVERE, e.getMessage());
+            System.exit(-1);
+        }
+
         //Instantiate VepVcf class and pass VCF file location
-        VepVcf vepVcf = new VepVcf(new File(args[0]));
+        VepVcf vepVcf = new VepVcf(new File(commandLine.getOptionValue("V")));
 
         //Open the file and parse the opened file
         vepVcf.parseVepVcf();
